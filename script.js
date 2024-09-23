526a231f9cdabb2397eb43ce3179fe9a96edbee8
law973 <lawright379@gmail.com>
2024-09-22 21:55:05 -0300

    finished JavaScript for theme switcher buttons

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-const blogImage = document.getElementById("blog-image");
 const redThemeSwitcher = document.getElementById("theme-switcher-red");
 const yellowThemeSwitcher = document.getElementById("theme-switcher-yellow");
 const greenThemeSwitcher = document.getElementById("theme-switcher-green");
 const blueThemeSwitcher = document.getElementById("theme-switcher-blue");
 
-function switchToRedTheme() {
-    blogImage.src = "./assets/images/illustration-article-red.svg";
-    document.documentElement.setAttribute('data-theme', 'red');
-}
+const switchBlogImageSource = themeColor => {
+    const blogImage = document.getElementById('blog-image');
+    const newSource = `./assets/images/illustration-article-${themeColor}.svg`;
+    blogImage.src = newSource;
+};
 
-function switchToYellowTheme() {
-    blogImage.src = "./assets/images/illustration-article-yellow.svg";
-    document.documentElement.setAttribute('data-theme', 'yellow');
-}
+const switchDataTheme = themeColor => {
+    document.documentElement.setAttribute('data-theme', themeColor);
+};
 
-function switchToGreenTheme() {
-    blogImage.src = "./assets/images/illustration-article-green.svg";
-    document.documentElement.setAttribute('data-theme', 'green');
-}
+const enableThemeSwitchers = () => {
+    document.querySelectorAll('button').forEach( button => { button.disabled = false; });
+};
 
-function switchToBlueTheme() {
-    blogImage.src = "./assets/images/illustration-article-blue.svg";
-    document.documentElement.setAttribute('data-theme', 'blue');
-}
+const disableCurrentThemeSwitcher = themeColor => {
+    switch (themeColor) {
+        case 'red':
+            redThemeSwitcher.disabled = true;
+            break;
+        case 'yellow':
+            yellowThemeSwitcher.disabled = true;
+            break;
+        case 'green':
+            greenThemeSwitcher.disabled = true;
+            break;
+        case 'blue':
+            blueThemeSwitcher.disabled = true;
+            break;
+    };
+};
 
-redThemeSwitcher.addEventListener('click', switchToRedTheme);
-yellowThemeSwitcher.addEventListener('click', switchToYellowTheme);
-greenThemeSwitcher.addEventListener('click', switchToGreenTheme);
-blueThemeSwitcher.addEventListener('click', switchToBlueTheme);
+const switchToNewTheme = themeColor => {
+    switchBlogImageSource(themeColor);
+    switchDataTheme(themeColor);
+    enableThemeSwitchers();
+    disableCurrentThemeSwitcher(themeColor);
+};
+
+// Event listeners
+redThemeSwitcher.addEventListener('click', () => switchToNewTheme('red'));
+yellowThemeSwitcher.addEventListener('click', () => switchToNewTheme('yellow'));
+greenThemeSwitcher.addEventListener('click', () => switchToNewTheme('green'));
+blueThemeSwitcher.addEventListener('click', () => switchToNewTheme('blue'));
+
+// Default theme on page load is yellow
+switchToNewTheme('yellow');
